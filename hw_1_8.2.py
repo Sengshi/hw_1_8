0b90c8edcf4b643ce4338bac07fd6f08e4e6ad44
aloster <asket2013@ya.ru>
2022-08-26 18:43:30 +1000

    Complete work 1_8.2 (update)

--- hw_1_8.2.py
+++ hw_1_8.2.py
@@ -12,20 +12,18 @@ class YaUploader:
         # Функция может ничего не возвращать
         url = 'https://cloud-api.yandex.net/v1/disk/resources'
         headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {token}'}
-        response = requests.get(f'{url}/upload?path={file_path}&overwrite=True', headers=headers).json()
-        # print(response, f'{file_path}')
+        response = requests.get(f'{url}/upload?path={path.basename(file_path)}&overwrite=True', headers=headers).json()
         with open(file_path, 'r') as f:
             try:
                 requests.put(response['href'], files={'file': f})
-                print(response)
+                print(f'Файл {path.basename(file_path)} успешно загружен!')
             except KeyError:
                 print(response)
 
 
 if __name__ == '__main__':
     # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = path.basename('/Users/aloster/PycharmProjects/hw_1_8/test.txt')
+    path_to_file = '/Users/aloster/PycharmProjects/hw_1_8/test.txt'
     token = ''
     uploader = YaUploader(token)
-    print(path_to_file)
     result = uploader.upload(path_to_file)
