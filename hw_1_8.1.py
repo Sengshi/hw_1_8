fb4f49e6e3dbd2851f0428a0df6f448417145940
aloster <asket2013@ya.ru>
2022-09-02 22:49:19 +1000

    Update work 1_8.1 (Если у нескольких персонажей один уровень интелекта)

--- hw_1_8.1.py
+++ hw_1_8.1.py
@@ -1,16 +1,14 @@
 import requests
-import json
 
 
 int_hero = {}
 heroes = ['Hulk', 'Captain America', 'Thanos']
 url = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json'
-response = json.loads(requests.get(url).text)
+response = requests.get(url).json()
 for hero in heroes:
     for parametrs in response:
         if hero == parametrs['name']:
             int_hero[parametrs['name']] = parametrs['powerstats']['intelligence']
-            break
 for k, v in int_hero.items():
     if v == max(int_hero.values()):
         print(f'Умнейший персонаж комиксов: {k}')
